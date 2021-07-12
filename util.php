5a9546e5273e120e8028fac8c8188d2dcab923fd
MAE0906 <96aiannmann@gmail.com>
2021-07-13 01:19:19 +0900

    ログイン機能実装

--- util.php
+++ util.php
@@ -1,12 +1,11 @@
 <?php
-
-///////////////////////
+///////////////////////////////////////
 // 便利な関数
-///////////////////////
-
+///////////////////////////////////////
+ 
 /**
- * 画像ファイル名から画像ファイルのURLを生成
- * 
+ * 画像ファイル名から画像のURLを生成
+ *
  * @param string $name 画像ファイル名
  * @param string $type ユーザー画像かツイート画像
  * @return string
@@ -16,14 +15,14 @@ function buildImagePath(string $name = null, string $type)
     if ($type === 'user' && !isset($name)) {
         return HOME_URL . 'Views/img/icon-default-user.svg';
     }
-
-    return HOME_URL . 'Views/img_uploaded/' . $type. '/' . htmlspecialchars($name);
+ 
+    return HOME_URL . 'Views/img_uploaded/' . $type . '/' . htmlspecialchars($name);
 }
-
+ 
 /**
  * 指定した日時からどれだけ経過したかを取得
- * 
- * @param strin $datetime
+ *
+ * @param string $datetime 日時
  * @return string
  */
 function convertToDayTimeAgo(string $datetime)
@@ -31,19 +30,19 @@ function convertToDayTimeAgo(string $datetime)
     $unix = strtotime($datetime);
     $now = time();
     $diff_sec = $now - $unix;
-
+ 
     if ($diff_sec < 60) {
         $time = $diff_sec;
-        $unix = '秒前';
-    } elseif ($diff_sec < 3600){
+        $unit = '秒前';
+    } elseif ($diff_sec < 3600) {
         $time = $diff_sec / 60;
-        $unix = '分前';
+        $unit = '分前';
     } elseif ($diff_sec < 86400) {
         $time = $diff_sec / 3600;
-        $unix = '時間前';
+        $unit = '時間前';
     } elseif ($diff_sec < 2764800) {
         $time = $diff_sec / 86400;
-        $unix = '日前';
+        $unit = '日前';
     } else {
         if (date('Y') != date('Y', $unix)) {
             $time = date('Y年n月j日', $unix);
@@ -52,6 +51,69 @@ function convertToDayTimeAgo(string $datetime)
         }
         return $time;
     }
-
-    return (int)$time . $unix;
+ 
+    return (int)$time . $unit;
+}
+ 
+/**
+ * ユーザー情報をセッションに保存
+ *
+ * @param array $user
+ * @return void
+ */
+function saveUserSession(array $user)
+{
+    // セッションを開始していない場合
+    if (session_status() === PHP_SESSION_NONE) {
+        // セッション開始
+        session_start();
+    }
+ 
+    $_SESSION['USER'] = $user;
+}
+ 
+/**
+ * ユーザー情報をセッションから削除
+ *
+ * @return void
+ */
+function deleteUserSession()
+{
+    // セッションを開始していない場合
+    if (session_status() === PHP_SESSION_NONE) {
+        // セッション開始
+        session_start();
+    }
+ 
+    // セッションのユーザー情報を削除
+    unset($_SESSION['USER']);
+}
+ 
+/**
+ * セッションのユーザー情報を取得
+ *
+ * @return array|false
+ */
+function getUserSession()
+{
+    // セッションを開始していない場合
+    if (session_status() === PHP_SESSION_NONE) {
+        // セッション開始
+        session_start();
+    }
+ 
+    if (!isset($_SESSION['USER'])) {
+        // セッションにユーザー情報がない
+        return false;
+    }
+ 
+    $user = $_SESSION['USER'];
+ 
+    // 画像のファイル名からファイルのURLを取得
+    if (!isset($user['image_name'])) {
+        $user['image_name'] = null;
+    }
+    $user['image_path'] = buildImagePath($user['image_name'], 'user');
+ 
+    return $user;
 }
