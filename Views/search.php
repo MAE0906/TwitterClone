2c4633b207ad2e9666a1dc8ace1614dd336197e9
MAE0906 <96aiannmann@gmail.com>
2021-07-24 18:59:52 +0900

    検索機能

--- Views/search.php
+++ Views/search.php
@@ -1,41 +1,3 @@
-<?php
-
-// 設定関連を読み込み読み込む
-include_once('../config.php');
-// 便利な関数を読み込む
-include_once('../util.php');
-
-/////////////////////////////////
-// ツイート一覧
-/////////////////////////////////
-$view_tweets = [
-    [
-        'user_id' => 1,
-        'user_name' => 'taro',
-        'user_nickname' => '太郎',
-        'user_image_name' => 'sample-person.jpg',
-        'tweet_body' => '今プログラミングをしています。',
-        'tweet_image_name' => null,
-        'tweet_created_at' => '2021-03-15 14:00:00',
-        'like_id' => null,
-        'like_count' => 0,
-    ],
-    [
-        'user_id' => 2,
-        'user_name' => 'jiro',
-        'user_nickname' => '次郎',
-        'user_image_name' => null,
-        'tweet_body' => 'コワーキングスペースをオープンしました。',
-        'tweet_image_name' => 'sample-post.jpg',
-        'tweet_created_at' => '2021-03-14 14:00:00',
-        'like_id' => 1,
-        'like_count' => 1,
-    ],
-
-];
-
-
-?>
 <!DOCTYPE html>
 <html lang="ja">
  
@@ -47,35 +9,35 @@ $view_tweets = [
  
 <body class="home search text-center">
     <div class="container">
-    <?php include_once('../Views/common/side.php'); ?>
+        <?php include_once('../Views/common/side.php'); ?>
+ 
         <div class="main">
             <div class="main-header">
                 <h1>検索</h1>
             </div>
-
+ 
             <form action="search.php" method="get">
                 <div class="search-area">
-                    <input type="text" class="form-control" placeholder="キーワード検索" name="keyword" value="">
+                    <input type="text" class="form-control" placeholder="キーワード検索" name="keyword" value="<?php echo htmlspecialchars($view_keyword); ?>">
                     <button type="submit" class="btn">検索</button>
                 </div>
             </form>
-
+ 
             <div class="ditch"></div>
  
-            <?php if (empty($view_tweets)): ?>
+            <?php if (empty($view_tweets)) : ?>
                 <p class="p-3">該当のツイートは見つかりませんでした</p>
-            <?php else: ?>
+            <?php else : ?>
                 <div class="tweet-list">
-                    <?php foreach ($view_tweets as $view_tweet): ?>
+                    <?php foreach ($view_tweets as $view_tweet) : ?>
                         <?php include('../Views/common/tweet.php'); ?>
                     <?php endforeach; ?>
                 </div>
             <?php endif; ?>
         </div>
     </div>
-
+ 
     <?php include_once('../Views/common/foot.php'); ?>
 </body>
  
 </html>
-
